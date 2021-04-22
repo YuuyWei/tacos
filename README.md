ba3f5e59ea1e772565b66b1a7601b5906af6a0dd
yuuy <weiyushuai666@gmail.com>
2021-04-22 21:59:57 +0800

    Chapter 1: Home Controller and Test

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a spring boot application example in **spring 5 in action**
---
+++ src/test/java/com/yuuy/tacos/controller/HomeControllerTest.java
@@ -0,0 +1,33 @@
+package com.yuuy.tacos.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+// RunWith is Replaced by ExtendWith
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(MockMvcResultMatchers.content().string(containsString("Welcome to...")));
+    }
+}
