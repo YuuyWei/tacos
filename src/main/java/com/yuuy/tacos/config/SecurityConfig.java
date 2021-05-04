538965b610098e7dc99129fae3c5d505052d9769
yuuy <weiyushuai666@gmail.com>
2021-05-04 16:47:46 +0800

    Chapter 4: user storage by JDBC

--- src/main/java/com/yuuy/tacos/config/SecurityConfig.java
+++ src/main/java/com/yuuy/tacos/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.yuuy.tacos.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,21 +9,21 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder())
-                    .withUser("buzz")
-                        .password("{noop}infinity")
-                        .authorities("ROLE_USER")
-                    .and()
-                    .withUser("woody")
-                        .password("{noop}bullseye")
-                        .authorities("ROLE_USER");
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder());
     }
 
     private static PasswordEncoder passwordEncoder() {
